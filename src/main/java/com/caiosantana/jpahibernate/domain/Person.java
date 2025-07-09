0b4a9dfb362e7f8741c1b38b36688a973b90d85a
Caio Santana <caiosantana.lkd@gmail.com>
2025-07-09 01:13:38 -0300

    Connect to db

--- src/main/java/com/caiosantana/jpahibernate/domain/Person.java
+++ src/main/java/com/caiosantana/jpahibernate/domain/Person.java
@@ -1,7 +1,22 @@
 package com.caiosantana.jpahibernate.domain;
 
-public class Person {
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.io.Serializable;
+
+@Entity
+public class Person implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
     private Integer id;
+
+
     private String nome;
     private String email;
 
--- src/main/java/com/caiosantana/jpahibernate/program/Program.java
+++ src/main/java/com/caiosantana/jpahibernate/program/Program.java
@@ -1,6 +1,9 @@
 package com.caiosantana.jpahibernate.program;
 
 import com.caiosantana.jpahibernate.domain.Person;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -13,7 +16,14 @@ public class Program {
 		Person p2 = new Person(null, "Joaquim Torres", "joaquim@gmail.com");
 		Person p3 = new Person(null, "Ana Maria", "ana@gmail.com");
 
-
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		em.getTransaction().commit();
+		System.out.println("Pronto");
 	}
 
 }
